c10fc27721ce4cf2b3705eca650944ea672fe7d9
Christian Meusel <christian.meusel@posteo.de>
2020-12-28 00:20:46 +0100

    Measure temperature with internal sensor

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -7,6 +7,7 @@ use embedded_hal::blocking::delay::DelayMs;
 use nrf52840_hal::{
     self as hal,
     gpio::{p0::Parts as P0Parts, Level},
+    Temp,
     Timer,
 };
 use switch_hal::{OutputSwitch, IntoSwitch};
@@ -20,12 +21,16 @@ fn main() -> ! {
     let pins = P0Parts::new(board.P0);
     let mut led_1 = pins.p0_13.into_push_pull_output(Level::High)
         .into_active_low_switch();
+    let mut temp = Temp::new(board.TEMP);
     let mut timer = Timer::new(board.TIMER0);
 
     defmt::info!("Turning LED on ...");
-
     led_1.on().unwrap();
     timer.delay_ms(1000u32);
+
+    defmt::info!("Measuring temperature ...");
+    let temperature = temp.measure();
+    defmt::info!("temperature: {:f32} °C", temperature.to_num());
 
     defmt::info!("Goodbye!");
 
