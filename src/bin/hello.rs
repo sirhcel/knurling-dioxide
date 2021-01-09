22e7d44190f060ee4bd28c5e993e6750ba5eb0f3
Christian Meusel <christian.meusel@posteo.de>
2021-01-10 00:04:10 +0100

    Light LED2 when Button 1 is pressed

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -10,7 +10,7 @@ use nrf52840_hal::{
     Temp,
     Timer,
 };
-use switch_hal::{OutputSwitch, IntoSwitch};
+use switch_hal::{OutputSwitch, InputSwitch, IntoSwitch};
 
 
 #[cortex_m_rt::entry]
@@ -21,8 +21,12 @@ fn main() -> ! {
     let pins = P0Parts::new(board.P0);
     let mut led_1 = pins.p0_13.into_push_pull_output(Level::High)
         .into_active_low_switch();
+    let mut led_2 = pins.p0_14.into_push_pull_output(Level::High)
+        .into_active_low_switch();
     let mut temp = Temp::new(board.TEMP);
     let mut timer = Timer::new(board.TIMER0);
+
+    let button_1 = pins.p0_11.into_pullup_input().into_active_low_switch();
 
     defmt::info!("Turning LED on ...");
     led_1.on().unwrap();
@@ -32,7 +36,17 @@ fn main() -> ! {
     let temperature = temp.measure();
     defmt::info!("temperature: {:f32} °C", temperature.to_num());
 
-    defmt::info!("Goodbye!");
+    defmt::info!("Entering LED loop ...");
 
-    dioxide::exit()
+    loop {
+        led_1.on().unwrap();
+        if button_1.is_active().unwrap() {
+            defmt::info!("Button 1 pressed");
+            led_2.on().unwrap();
+        }
+        timer.delay_ms(500u32);
+        led_1.off().unwrap();
+        led_2.off().unwrap();
+        timer.delay_ms(500u32);
+    }
 }
