0a37d23bee0cdd4e5c7aafcbf82b56adb798ee85
Christian Meusel <christian.meusel@posteo.de>
2021-02-05 22:07:38 +0100

    Add custom error (wrapper) to SCD30 driver

--- src/scd30.rs
+++ src/scd30.rs
@@ -11,6 +11,20 @@ use defmt::Format;
 
 
 
+// A custom error type for reporting errors from both, the driver itself and
+// the underlying I2C implementation.
+//
+// TODO: I see that Debug should be derived for printing backtraces. What about
+// Copy and Clone? Which traits should be implemented by errors? And what about
+// Eq and PartialEq? Are they meant for collections? Or are they required for
+// matching?
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum Error<E> {
+    CrcError,
+    I2cError(E),
+}
+
+
 // TODO: How to be agnostic of any formatting stuff while being able to output
 // this struct via defmt?
 #[derive(Format)]
@@ -32,13 +46,22 @@ pub const I2C_ADDRESS: u8 = 0x61;
 
 
 
+// Allow automatic conversion from the I2C implementation's error type to the
+// driver's error type (for the question mark operator).
+impl<E> From<E> for Error<E> {
+    fn from(err: E) -> Error<E> {
+        Error::I2cError(err)
+    }
+}
+
+
 impl<I2C, E> Scd30<I2C> where I2C: Read<Error = E> + Write<Error = E> {
     pub fn new(i2c: I2C) -> Self {
         Scd30{ i2c }
     }
 
 
-    pub fn get_firmware_version(&mut self) -> Result<FirmwareVersion, E> {
+    pub fn get_firmware_version(&mut self) -> Result<FirmwareVersion, Error<E>> {
         let command: [u8; 2] = [0xd1, 0x00];
         let mut response = [0u8; 3];
 
@@ -57,9 +80,7 @@ impl<I2C, E> Scd30<I2C> where I2C: Read<Error = E> + Write<Error = E> {
         if response_crc == our_crc {
             Ok(FirmwareVersion{ major, minor })
         } else {
-            // FIXME: How to return a custom SCD30 error in case of a failed
-            // CRC check?
-            Ok(FirmwareVersion{ major: 0, minor: 0 })
+            Err(Error::<E>::CrcError)
         }
     }
 
