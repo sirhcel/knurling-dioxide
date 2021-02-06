b5500afbde20edb0d3e5d451f21e4e70e47f5de6
Christian Meusel <christian.meusel@posteo.de>
2021-02-06 23:00:41 +0100

    Print first text on e-paper display

--- src/bin/hello.rs
+++ src/bin/hello.rs
@@ -4,12 +4,25 @@
 
 use dioxide as _; // global logger + panicking-behavior + memory layout
 use dioxide::scd30;
+use embedded_graphics::{
+    fonts::{Font24x32, Text},
+    geometry::Point,
+    pixelcolor::BinaryColor,
+    prelude::*,
+    style::TextStyle,
+};
 use embedded_hal::blocking::delay::DelayMs;
+use epd_waveshare::{
+    epd4in2::*,
+    graphics::Display,
+    prelude::*,
+};
 use nrf52840_hal::{
     Temp,
     Timer,
-    gpio::{p0::Parts as P0Parts, Level},
+    gpio::{p0::Parts as P0Parts, p1::Parts as P1Parts, Level},
     self as hal,
+    spim::{self, Spim},
     twim::{self, Twim},
 };
 use switch_hal::{OutputSwitch, InputSwitch, IntoSwitch};
@@ -20,21 +33,35 @@ fn main() -> ! {
     defmt::info!("Hello, world!");
 
     let board = hal::pac::Peripherals::take().unwrap();
-    let pins = P0Parts::new(board.P0);
-    let mut led_1 = pins.p0_13.into_push_pull_output(Level::High)
+    let pins_0 = P0Parts::new(board.P0);
+    let pins_1 = P1Parts::new(board.P1);
+    let mut led_1 = pins_0.p0_13.into_push_pull_output(Level::High)
         .into_active_low_switch();
-    let mut led_2 = pins.p0_14.into_push_pull_output(Level::High)
+    let mut led_2 = pins_0.p0_14.into_push_pull_output(Level::High)
         .into_active_low_switch();
     let mut temp = Temp::new(board.TEMP);
     let mut timer = Timer::new(board.TIMER0);
 
-    let button_1 = pins.p0_11.into_pullup_input().into_active_low_switch();
+    let button_1 = pins_0.p0_11.into_pullup_input().into_active_low_switch();
 
-    let scl = pins.p0_30.degrade();
-    let sda = pins.p0_31.degrade();
+    let scl = pins_0.p0_30.degrade();
+    let sda = pins_0.p0_31.degrade();
     let i2c_pins = twim::Pins{ scl, sda };
     let i2c = Twim::new(board.TWIM0, i2c_pins, twim::Frequency::K100);
     let mut sensor = scd30::Scd30::new(i2c);
+
+    // TODO: Why do we need to degrade two of the pins?
+    let din = pins_1.p1_01.into_push_pull_output(Level::Low).degrade();
+    let clk = pins_1.p1_02.into_push_pull_output(Level::Low).degrade();
+    let cs = pins_1.p1_03.into_push_pull_output(Level::Low);
+    let dc = pins_1.p1_04.into_push_pull_output(Level::Low);
+    let rst = pins_1.p1_05.into_push_pull_output(Level::Low);
+    let busy = pins_1.p1_06.into_floating_input();
+    let spi_pins = spim::Pins{ sck: clk, miso: None, mosi: Some(din) };
+    let mut spi = Spim::new(board.SPIM3, spi_pins, spim::Frequency::K500, spim::MODE_0, 0);
+    let mut epd_timer = Timer::new(board.TIMER1);
+    let mut epd = EPD4in2::new(&mut spi, cs, busy, dc, rst, &mut epd_timer).unwrap();
+
 
     defmt::info!("Turning LED on ...");
     led_1.on().unwrap();
@@ -48,6 +75,15 @@ fn main() -> ! {
     defmt::info!("SCD30 firmware version: {:?}", sensor_fw_version);
     let pressure_mbar = 1020_u16;
     sensor.start_continuous_measurement(pressure_mbar).unwrap();
+
+
+    let mut display = Display4in2::default();
+    Text::new("Hello Knurling!", Point::new(20, 30))
+        .into_styled(TextStyle::new(Font24x32, BinaryColor::On))
+        .draw(&mut display)
+        .unwrap();
+    epd.update_frame(&mut spi, &display.buffer()).unwrap();
+    epd.display_frame(&mut spi).expect("display frame new graphics");
 
 
     defmt::info!("Entering loop ...");
